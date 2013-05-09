63b5a366933381d35b6cb2a5a5b0c24626f978b7
shiliming <s_2305439@163.com>
2013-05-09 17:37:20 +0800

    加入中文注释

--- sysop.py
+++ sysop.py
@@ -10,6 +10,7 @@ class SystemOp(object):
     """
     Reurn a database result
     @rtype: The result of the database
+    测试方法，测试该session_id是否在登陆状态。
     """
     con = db.connect("test.db")
     cur=con.cursor()
@@ -27,6 +28,7 @@ class SystemOp(object):
     """
     login function
     @rtype:session_id
+    登陆，输入用户名密码，返回session_id。
     """
     session_id=hash(user+password)
     con = db.connect("test.db")
@@ -38,6 +40,11 @@ class SystemOp(object):
 
   @ladonize(str,rtype=bool)
   def logout(self,session_id):
+    """
+    logout function
+    @rtype:bool
+    注销账户，注销成功则返回True。
+    """
     con = db.connect("test.db")
     cur=con.cursor()
     res=cur.execute('delete  from m_test  where session_id=?',(session_id,))
